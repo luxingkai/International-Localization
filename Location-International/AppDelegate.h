365696d6618d8043f37e02f80f86cba9406a451f
luxingkai <18356285335@163.com>
2020-05-08 19:14:51 +0800

    Initial Commit

---
+++ Location-International/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  Location-International
+//
+//  Created by tiger fly on 2020/5/8.
+//  Copyright © 2020 tiger fly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Location-InternationalTests/Location_InternationalTests.m
@@ -0,0 +1,37 @@
+//
+//  Location_InternationalTests.m
+//  Location-InternationalTests
+//
+//  Created by tiger fly on 2020/5/8.
+//  Copyright © 2020 tiger fly. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Location_InternationalTests : XCTestCase
+
+@end
+
+@implementation Location_InternationalTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
